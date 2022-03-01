9ef4d2182efd9deca31c4da27c1d0e514e9e5342
makturner <98291417+makturner@users.noreply.github.com>
2022-03-01 14:25:46 -0500

    Add files via upload

---
+++ Mar 1.R
@@ -0,0 +1,10 @@
+#Author: Makayla Turner, Date: Feb 22, 2022, Purpose: To generate box plots for raw microarray datasets
+setwd("/Users/makaylaturner/Desktop/GSE344_RAW1")
+library(affy)
+eset <- ReadAffy()
+# Perform RMA normalization
+eset_Norm <- rma(eset)
+rma <- exprs(eset_Norm)
+#Log base 2, or base 10, To perform log transformation do following
+Treatment <- apply(rma[,c("GSM4843.CEL", "GSM4844.CEL")],1,log)
+Control <- apply(rma[,c("GSM4845.CEL","GSM4846.CEL","GSM4847.CEL")],1,log)
